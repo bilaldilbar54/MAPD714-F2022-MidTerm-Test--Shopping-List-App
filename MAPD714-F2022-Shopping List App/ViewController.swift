217b922af8fefd87c2e5ba539ce13d0d3b98e7ae
Muhammad Bilal <bilal.dilbar54@gmail.com>
2022-10-27 14:12:14 -0400

    first commit

---
+++ MAPD714-F2022-Shopping List App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD714-F2022-Shopping List App
+//
+//  Created by Muhammad Bilal Dilbar on 2022-10-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
