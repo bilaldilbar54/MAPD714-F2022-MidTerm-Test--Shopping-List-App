cc5ff0f6104b71836813b08292996620ae2f84d1
Muhammad Bilal <bilal.dilbar54@gmail.com>
2022-10-27 15:02:02 -0400

    first commit

--- MAPD714-F2022-Shopping List App/ViewController.swift
+++ MAPD714-F2022-Shopping List App/ViewController.swift
@@ -1,9 +1,10 @@
-//
-//  ViewController.swift
-//  MAPD714-F2022-Shopping List App
-//
-//  Created by Muhammad Bilal Dilbar on 2022-10-27.
-//
+//  Midterm Test - Simple Shopping List App
+//  Author's name and StudentID:
+//  Muhammad Bilal Dilbar Hussain
+//  Student ID: 301205152
+//  Dated: 26/Oct/2022
+//  App description: This is a simple shopping list app that allows user to enter the list name followed by the list items with their prices and quantities.
+//  Xcode Version : Version 14.0.1 (14A400)
 
 import UIKit
 
